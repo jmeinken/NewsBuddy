335fe609563cb2beeab73583a6beaef77d07ed9e
jmeinken <thebellsofohio@hotmail.com>
2017-06-11 22:38:35 -0400

    include read time

--- main/models.py
+++ main/models.py
@@ -8,7 +8,7 @@ from django.db import models
 # Create your models here.
 
 class NewsFeed(models.Model):
-
+ 
     url = models.URLField()
     title = models.CharField(max_length=500)
     author = models.CharField(max_length=500, blank=True, null=True)
@@ -18,6 +18,19 @@ class NewsFeed(models.Model):
     article_time = models.TimeField(max_length=500, blank=True, null=True)
     created_on = models.DateTimeField(default=datetime.datetime.now)
     published = models.BooleanField(default=True)
+    read_time = models.IntegerField(blank=True, null=True)
+    
+    PRINT = 'print'
+    VIDEO = 'video'
+    AUDIO = 'audio'
+    INTERACTIVE = 'interactive'
+    MEDIA_TYPE_CHOICES = (
+        (PRINT, 'print'),
+        (VIDEO, 'video'),
+        (AUDIO, 'audio'),
+        (INTERACTIVE, 'interactive'),
+    )
+    media_type = models.CharField(max_length=60, choices=MEDIA_TYPE_CHOICES, default=PRINT,)
     
     class Meta:
         ordering = ['-created_on', '-article_date', '-article_time']
